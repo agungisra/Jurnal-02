75817731bb2b06ec0135bda1be30b3a7d4ab8dce
agungisra <agungisra27@gmail.com>
2021-09-30 20:50:42 +0700

    HelloWorld.java

---
+++ Jurnal 01/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: M. Agung Isra Narwin");
+		System.out.println("NIM	: 6706213022");
+		System.out.println("Kelas	: D3IF-45-05");
+	}
+}
